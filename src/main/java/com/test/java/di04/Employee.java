296a1633a0d80232aca6a9d78dc97db39e7b0161
Sunmin <sunmin323233@gmail.com>
2025-10-20 15:27:32 +0900

    Dependency Injection 예제 4

---
+++ src/main/java/com/test/java/di04/Employee.java
@@ -0,0 +1,7 @@
+package com.test.java.di04;
+
+public interface Employee {
+	//직원이 할 행동(interface)
+	//홍길동 or 이순신 -> 각각의 개인이 아닌 직원(Employee)로서 해야할 일 정의
+	void work();
+}
---
+++ src/main/java/com/test/java/di04/Service.java
@@ -0,0 +1,32 @@
+package com.test.java.di04;
+
+public class Service {
+	
+	private Employee employee;
+	
+	
+//	public Service(Employee employee) { //생성자 의존 주입 도구
+//		this.employee = employee;
+//	}
+	
+	//setter를 이용한 의존 주입
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	
+	public void doSomething() {
+		//서비스 자신의 업무
+		System.out.println("자료를 취합");
+
+		//DI
+		//hong.work();
+		//lee.work();
+		employee.work();
+		
+		
+		//서비스 자신의 업무
+		System.out.println("자료를 전송");
+	}
+
+
+}
